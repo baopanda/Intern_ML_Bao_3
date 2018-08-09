b4f6781c1651e27fda3ff09335dbe92df878bff8
baopanda <32414490+baopanda@users.noreply.github.com>
2018-08-10 00:04:54 +0700

    dsada
    dsadas

--- Bai4/bai4.py
+++ Bai4/bai4.py
@@ -1,21 +1,38 @@
-import datetime
+# import datetimetime
 import pandas as pd
 import matplotlib.pyplot as plt
 from math import *
+from datetime import datetime
 
 def ConvertTime(a):
-    b = datetime.datetime.fromtimestamp(int(a)).strftime('%Y-%m-%d %H:%M:%S')
+    b = datetime.fromtimestamp(int(a)).strftime('%Y-%m-%d %H:%M:%S')
+    # b = datetime.fromtimestamp(int(a)).strftime('%d-%m-%Y')
     return b
 
 def MinDistance(a,j):
     distance1 = sqrt(pow((a['Long'][1] - a['Long'][0]), 2) + pow((a['Latt'][1] - a['Latt'][0]), 2))
-    # print(distance1)
     for i in range(2, len(a['UnixTime'])):
         distance2 = sqrt(pow((a['Long'][i] - a['Long'][i-1]), 2) + pow((a['Latt'][i] - a['Latt'][i-1]), 2))
-        if (distance2 < distance1):
-            # print(distance2)
+        if (distance2 < distance1 and distance2!=0):
             distance1 = distance2
+            # print(distance1)
     print("Khoang cach min cua con %s là: " % label[j] + str(distance1) + " voi time là: "+ a['Time'][i]+" va "+ a['Time'][i-1])
+
+def MinDistance1(a,j):
+    distance1 = 10000000000
+    date_format = "%d-%m-%Y"
+    for i in range(0,len(a['UnixTime'])):
+        for j in range(i+1,len(a['UnixTime'])):
+            d0 = datetime.strptime(a['Time'][j], date_format)
+            d1 = datetime.strptime(a['Time'][i], date_format)
+            delta = abs(d0-d1)
+            if(delta.days == 1):
+                distance2 = sqrt(pow((a['Long'][j] - a['Long'][i]), 2) + pow((a['Latt'][j] - a['Latt'][i]), 2))
+                if(distance2<distance1):
+                    distance1=distance2
+                    print(distance1)
+
+    print("Khoang cach min cua con %s là: " % label[j] + str(distance1))
 
 def set_Index(a):
     count = []
@@ -27,28 +44,20 @@ if __name__ == '__main__':
     file = "ZebraBotswana.txt"
 
     df = pd.read_csv(file, delimiter=",")
-    # print(df)
-    # print(len(df['UnixTime']))
 
     df.sort_values('UnixTime', inplace=True)
 
-    # print(df)
     time = []
     for i in range(0, len(df['UnixTime'])):
         time.append(ConvertTime(df['UnixTime'][i]))
 
-    # print(time)
     df['Time'] = pd.Series(time, index=df.index)
-    # print(df)
 
     label = []
 
     for i in range(0, len(df['Animal'])):
         if df['Animal'][i] not in label:
             label.append(df['Animal'][i])
-    # print(label)
-    # print(len(label))
-    # print(label[6])
 
     a0 = df.query("Animal == '%s'" % label[0])
     a1 = df.query("Animal == '%s'" % label[1])
@@ -58,11 +67,6 @@ if __name__ == '__main__':
     a5 = df.query("Animal == '%s'" % label[5])
     a6 = df.query("Animal == '%s'" % label[6])
 
-    # data = []
-    # with open("output.txt", "w", encoding='utf-8') as file:
-    #     for i in range(0,len(a0['Time'])):
-    #         file.write(str(a0['UnixTime'][i])+"\t"+str(a0['Time'][i])+"\n")
-
     plt.plot(a0['Long'], a0['Latt'], '#16a085', linewidth=0.5, label=label[0])
     plt.plot(a1['Long'], a1['Latt'], '#c0392b', linewidth=0.5, label=label[1])
     plt.plot(a2['Long'], a2['Latt'], '#3498db', linewidth=0.5, label=label[2])
@@ -71,13 +75,13 @@ if __name__ == '__main__':
     plt.plot(a5['Long'], a5['Latt'], '#FFFF00', linewidth=0.5, label=label[5])
     plt.plot(a6['Long'], a6['Latt'], '#FF00FF', linewidth=0.5, label=label[6])
 
-    plt.legend(loc=1)
-    # plt.title("Number of animals each year")
-    # plt.xlabel("Years")
-    # plt.xticks(a, years)
-    # plt.show()
+    data = []
+    with open("output.txt", "w", encoding='utf-8') as file:
+        for i in range(0,len(a0['Time'])):
+            file.write(str(a0['Long'][i])+"\t"+str(a0['Latt'][i])+"\t"+str(a0['Time'][i])+"\n")
 
-    set_Index(a0)
+    plt.legend(loc=1)
+
     MinDistance(a0, 0)
     set_Index(a1)
     MinDistance(a1, 1)
@@ -93,7 +97,3 @@ if __name__ == '__main__':
     MinDistance(a6, 6)
 
     plt.show()
-
-
-
-
